8ac04b65d409f9fc402e610c475f9d424cf63b22
Ettore Senatore <47864270+EttoreCS@users.noreply.github.com>
2019-03-06 21:06:48 -0800

    Add files via upload

---
+++ Bishop.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+public class Bishop extends ChessPiece {
+
+    public static final int BISHOP_IMPORTANCE = 3;
+    public Bishop() {
+        super(BISHOP_IMPORTANCE);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "(" + getValue() + ")";
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Diagonally");
+    }
+}
